12d8d8217911dcee32dc37f0877b1dbf62e48fc4
SomVariable <79557545+SomVariable@users.noreply.github.com>
2022-09-27 16:19:36 +0300

    project initialization

---
+++ src/DnDSection.js
@@ -0,0 +1,12 @@
+import Input from "./UI/Input/Input";
+
+function DnDSection() {
+  return (
+    <div className="App">
+       <Input />
+       
+    </div>
+  );
+}
+
+export default DnDSection;
